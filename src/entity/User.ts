20d68d9336f778f9f4725c284c9fc9d44d7c99f2
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-06-02 16:53:43 +0100

    Feature/restapi shell (#1)
    * Edit readme
    
    * Updated readme title
    
    * basic setup of routes and controllers
    
    * typeorm migration:create -n CreateAdminUser
    
    * added admin user up
    
    * npm start to populate database
    
    * npm run migration:run
    
    * cleanup bad auth header
    
    * a few postman scripts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,45 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    CreateDateColumn,
+    UpdateDateColumn
+} from "typeorm";
+import { Length, IsNotEmpty } from "class-validator";
+import * as bcrypt from "bcryptjs";
 
 @Entity()
+@Unique(["username"])
 export class User {
     @PrimaryGeneratedColumn()
     public id: number;
 
     @Column()
-    public firstName: string;
+    @Length(4, 20)
+    public username: string;
 
     @Column()
-    public lastName: string;
+    @Length(4, 100)
+    public password: string;
 
     @Column()
-    public age: number;
+    @IsNotEmpty()
+    public role: string;
+
+    @Column()
+    @CreateDateColumn()
+    public createdAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    private updatedAt: Date;
+
+    public hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
+
+    public checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+        return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
 }
